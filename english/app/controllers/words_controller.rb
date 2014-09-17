87fba2e71e4d786eac6bf619bc86bbb52d997b7a
root <root@ip-172-31-14-121.ap-northeast-1.compute.internal>
2014-09-17 01:28:35 +0000

    add edit function

--- english/app/controllers/words_controller.rb
+++ english/app/controllers/words_controller.rb
@@ -12,5 +12,17 @@ class WordsController < ApplicationController
     redirect_to '/words'
   end
 
+  def edit
+    @word = Word.find(params[:id])
+    @words = Word.all
+  end
+
+  def update
+    @word = Word.find(params[:word][:id])
+    @word.voc = params[:word][:voc]
+    @word.mean = params[:word][:mean]
+    @word.save
+    redirect_to :action => 'index'
+  end
 
 end
